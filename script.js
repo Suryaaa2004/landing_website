72c51f99fe41dd75fec97cf2dbfa394dd766d988
Surya Abhiram <128978667+Suryaaa2004@users.noreply.github.com>
2025-05-13 12:10:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+  alert("Thanks for exploring BizPro!");
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  alert("Your message has been sent. We'll get back to you soon!");
+}
